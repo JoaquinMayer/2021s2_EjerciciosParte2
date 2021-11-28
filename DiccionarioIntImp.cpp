61d29339d03fe16c45df55b0a0ed8b9d921fe511
Joaquin Mayer <joakinmayer96@gmail.com>
2021-11-28 20:20:38 -0300

    Comment

--- DiccionarioIntImp.cpp
+++ DiccionarioIntImp.cpp
@@ -2,14 +2,41 @@
 
 #ifdef DICCIONARIO_INT_IMP
 
+struct NodoDiccionarioInt {
+	int dato;
+	NodoDiccionarioInt* sig;
+	NodoDiccionarioInt(int d) : dato(d), sig(NULL) {}
+};
+
+// FUNCIONES AUXILIARES
+
+//PRE:
+//POS:
+// PRE:
+// POS: Agrega el dato creando un nuevo nodo
+void agregar(NodoDiccionarioInt*& d, int dato) {
+	if (d == NULL) {
+		d = new NodoDiccionarioInt(dato);
+	}
+	else { 
+		agregar(d->sig, dato);
+	}
+}
+
+// FIN FUNCIONES AUXILIARES
+
 struct _cabezalDiccionarioInt {
-	// NO IMPLEMENTADO
+	/*NodoDiccionarioInt* ppio;
+	unsigned int largo;*/
 };
 
 
 DiccionarioInt crearDiccionarioInt(unsigned int esperados) {
-	// NO IMPLEMENTADO
-	return NULL;
+	
+	/*DiccionarioInt d = new _cabezalDiccionarioInt();
+	d->ppio = NULL;
+	d->largo = 0;
+	return d;*/
 }
 
 void agregar(DiccionarioInt& d, int e) {
@@ -26,17 +53,18 @@ bool pertenece(DiccionarioInt d, int e) {
 }
 
 int elemento(DiccionarioInt d) {
+	/*assert(!esVacio(d));*/
 	// NO IMPLEMENTADO
 	return 0;
 }
 
 bool esVacio(DiccionarioInt d) {
-	// NO IMPLEMENTADO
+	/*return d->largo == 0;*/
 	return true;
 }
 
 unsigned int cantidadElementos(DiccionarioInt d) {
-	// NO IMPLEMENTADO
+	//return d->largo;
 	return 0;
 }
 
